46460a6dc33ff2d60ec36a0523cba9c746f3f4d0
mocitheccat <mocitheccat@gmail.com>
2024-04-07 14:03:59 +0200

    Fifth task - DONE

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,15 @@
+const changeColorBtn = document.querySelector(".change-color");
+const textOfColor = document.querySelector(".color");
+changeColorBtn.addEventListener("click", changeColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function changeColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  textOfColor.textContent = randomColor;
 }
