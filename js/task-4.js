4b598e3daed18575a0b83a3c658c4bf3997ca296
mocitheccat <mocitheccat@gmail.com>
2024-04-07 13:56:08 +0200

    Fourth task - DONE

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", onSubmit);
 
+function onSubmit(e) {
+  e.preventDefault();
+  const emailEL = e.target.elements.email;
+  const passwordEL = e.target.elements.password;
+
+  if (!emailEL.value || !passwordEL.value) {
+    alert("All form fields must be filled in");
+    return;
+  }
+  console.log({
+    [emailEL.name]: emailEL.value,
+    [passwordEL.name]: passwordEL.value,
+  });
+  e.target.reset();
+}
