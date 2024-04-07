0d05b556a5a9f3b64208e1581c35a3f60eca9a16
mocitheccat <mocitheccat@gmail.com>
2024-04-07 15:01:07 +0200

    Sixth task - Done

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,37 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const INITIAL_SIZES = 30;
+
+const boxesRef = document.querySelector("#boxes");
+const createBoxBtn = document.querySelector("button[data-create]");
+const destroyBoxBtn = document.querySelector("button[data-destroy]");
+const inputRef = document.querySelector('input[type="number"]');
+
+createBoxBtn.addEventListener("click", () => createBoxes(inputRef.value));
+
+destroyBoxBtn.addEventListener("click", (e) => {
+  boxesRef.innerHTML = "";
+});
+
+function createBoxes(amount) {
+  if (isNaN(amount) || amount <= 0 || amount > 100) {
+    alert("Please enter a positive number: 1 - 100");
+    return;
+  }
+
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.style.width = `${INITIAL_SIZES + i * 10}px`;
+    box.style.height = `${INITIAL_SIZES + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    fragment.appendChild(box);
+  }
+
+  boxesRef.appendChild(fragment);
+  inputRef.value = "";
+}
